ffc0cec31d045632aca3ff763c29a9b127cfddb4
haeun82 <cjwd829@gmail.com>
2023-11-21 17:44:57 +0900

    os 메서드 주석 추가

--- 12_Node.js/ch3/3.5/hash.js
+++ 12_Node.js/ch3/3.5/hash.js
@@ -5,7 +5,7 @@ const crypto = require('crypto');
 // 비밀번호는 서버(DB)에 그대로 저장하는건 위험!
 // 암호화해서 저장하는게 좋음 => 그래야 DB가 털려도 원래 비번을 알 수 없음
 // 정확히는 해시 기법: 문자열을 고정된 길이에 다른 문자열로 변환
-// 다양한 해시 알고리즘: md5, sha1, sha256, sha512, sha3, pbkdf2, bcrypt, scrypt 등
+// 다양한 해시 알고리즘: md5, sha1, sha256,(<- 잘 안씀) / sha512, sha3, pbkdf2, bcrypt, scrypt 등
 console.log('base64:', crypto.createHash('sha512').update('비밀번호').digest('base64'));
 console.log('hex:', crypto.createHash('sha512').update('비밀번호').digest('hex'));
 console.log('base64:', crypto.createHash('sha512').update('다른 비밀번호').digest('base64'));
--- 12_Node.js/ch3/3.5/os.js
+++ 12_Node.js/ch3/3.5/os.js
@@ -6,18 +6,18 @@ console.log('운영체제 정보---------------------------------');
 console.log('os.arch():', os.arch());
 console.log('os.platform():', os.platform());
 console.log('os.type():', os.type());
-console.log('os.uptime():', os.uptime());
-console.log('os.hostname():', os.hostname());
-console.log('os.release():', os.release());
+console.log('os.uptime():', os.uptime()); // 운영체제 부팅 이후 흐른 시간(초)을 보여줌
+console.log('os.hostname():', os.hostname()); // 컴퓨터의 이름을 보여줌
+console.log('os.release():', os.release());// 운영체제 버전 보여줌
 
 console.log('경로------------------------------------------');
-console.log('os.homedir():', os.homedir());
-console.log('os.tmpdir():', os.tmpdir());
+console.log('os.homedir():', os.homedir()); // 홈 디렉터리 경로를 보여줌
+console.log('os.tmpdir():', os.tmpdir()) // 임시 파일 저장 경로 보여줌
 
 console.log('cpu 정보--------------------------------------');
 console.log('os.cpus():', os.cpus()); // 컴퓨터의 코어 정보
 console.log('os.cpus().length:', os.cpus().length); // 코어의 갯수 / 컴퓨터의 코어 갯수만큼 서버를 띄우고 싶을때 활용
 
 console.log('메모리 정보-----------------------------------');
-console.log('os.freemem():', os.freemem());
-console.log('os.totalmem()', os.totalmem());
+console.log('os.freemem():', os.freemem()); // 사용 가능한 메모리(RAM)를 보여줌
+console.log('os.totalmem()', os.totalmem()); // 전체 메모리 용량
