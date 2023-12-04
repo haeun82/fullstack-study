72a184ad41eda72670e20bd5095a316e67f984e2
haeun82 <cjwd829@gmail.com>
2023-12-04 17:31:20 +0900

    회원가입, 로그인

--- 12_Node.js/haeunboard/app.js
+++ 12_Node.js/haeunboard/app.js
@@ -4,16 +4,23 @@ const morgan = require('morgan');
 const dotenv = require('dotenv');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
+const passport = require('passport');
+
 
 dotenv.config();
 
 // 라우터 가져오기
 const indexRouter = require('./routes');
 const postRouter = require('./routes/post');
+const userRouter = require('./routes/user');
+
 // DB 연결 함수 가져오기
 const { connect } = require('./database');
+// ./passport/index.js 가져오기
+const passportConfig = require('./passport');
 
 const app = express();
+passportConfig(); //패스포트 설정 실행
 app.set('port', process.env.PORT || 8080);
 app.set('view engine', 'ejs'); // view engine의 확장자 지정
 connect(); //몽고디비에 연결
@@ -33,10 +40,22 @@ app.use(session({
   },
   name: 'session-cookie'
 }));
+// passport 미들웨어 설정
+app.use(passport.initialize()); // 요청 객체에 passport 설정을 심음(req.inAuthenticated, req.login, req.logout 등)
+app.use(passport.session()); // req.session 객체에 passport 정보를 저장
+// req.session 객체는 express-session에서 생성하는 것이므로 passport 미들웨어는 express-session 미들웨어보다 뒤에 연결해야함
+
+// res.locals.user 속성에 req.user 정보 넣기를 미들웨어로 등록
+// 이유는? => 템플릿 엔진에서 user 객체를 통해 로그인한 사용자 정보에 접근할 수 있음
+app.use((req, res, next) => {
+  res.locals.user = req.user;
+  next();
+});
 
 // 라우터를 미들웨어로 등록
 app.use('/', indexRouter);
 app.use('/post', postRouter);
+app.use('/user', userRouter);
 
 app.use((req, res, next) => {
   const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
